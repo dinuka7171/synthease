7ac3ebab54355018b7a8cdff402a44f544692497
dinuka7171 <dinukaweerakoon625@gmail.com>
2025-02-25 21:40:34 +0530

    feat:task-ps-1

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,13 @@
+import productImage from "@/assets/product-image.png"
+
 export const ProductShowcase = () => {
-  return null;
+  return <section>
+    <div className="container">
+      <div>Boost your productivity</div>
+      <h2>A more effective way to track progress</h2>
+      <p>
+        Effortlessly turn your ideas into a fully functional, responsive, SaaS website in just minutes with this template.
+      </p>
+    </div>
+  </section> ;
 };
