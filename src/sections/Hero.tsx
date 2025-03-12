85f3da9135df3d30376f0287499d0552eb5b84b1
dinuka7171 <dinukaweerakoon625@gmail.com>
2025-03-12 17:05:59 +0530

    feat:animation-complited

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,19 +1,39 @@
+"use client";
 import ArrowIcon from "@/assets/arrow-right.svg";
-import cogImage  from "@/assets/cog.png";
+import cogImage from "@/assets/cog.png";
 import cylinderImage from "@/assets/cylinder.png";
 import noodleImage from "@/assets/noodle.png";
-import Image from 'next/image';
+import Image from "next/image";
+import {
+  motion,
+  useScroll,
+  useTransform,
+  useMotionValueEvent,
+} from "framer-motion";
+import { useRef } from "react";
 
 export const Hero = () => {
+  const heroRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: heroRef,
+    offset: ["start end", "end start"],
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
+  useMotionValueEvent(translateY, "change", (latestValue) =>
+    console.log(latestValue)
+  );
+
   return (
-    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
+    <section
+      ref={heroRef}
+      className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip"
+    >
       <div className="container">
-        <div className="md:flex items-center ">
-          <div className="md:w-[478px]" >
-            <div className="tag" >
-              Version 2.0 is here
-            </div>
-            <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6 ">
+        <div className="md:flex items-center">
+          <div className="md:w-[478px]">
+            <div className="tag">Version 2.0 is here</div>
+            <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
               Pathway to productivity
             </h1>
             <p className="text-xl text-[#010D3E] tracking-tight mt-6">
@@ -30,24 +50,42 @@ export const Hero = () => {
             </div>
           </div>
           <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
-            <Image
-             src={cogImage}
-              alt="Cog image" 
+            <motion.div
+              animate={{ translateY: [-30, 30] }}
+              transition={{
+                repeat: Infinity,
+                repeatType: "mirror",
+                duration: 3,
+                ease: "easeInOut",
+              }}
               className="md:absolute md:h-full md:w-auto md:max-w-none md:left-6 lg:left-0"
-              />
-              <Image 
-              src={cylinderImage} 
-              width={220} 
-              height={220} 
-              alt="cylinderImage"
+            >
+              <Image src={cogImage} alt="Cog image" priority />
+            </motion.div>
+            <motion.div
               className="hidden md:block -top-8 -left-32 md:absolute"
+              style={{ translateY }}
+            >
+              <Image
+                src={cylinderImage}
+                width={220}
+                height={220}
+                alt="Cylinder Image"
+                priority
               />
-              <Image 
-              src={noodleImage}
-              width={220}
-              alt="Noodle image"
-               className="hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]"
+            </motion.div>
+            <motion.div
+              className="hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]"
+              style={{ rotate: 30, translateY }}
+            >
+              <Image
+                src={noodleImage}
+                width={220}
+                height={220}
+                alt="Noodle Image"
+                priority
               />
+            </motion.div>
           </div>
         </div>
       </div>
